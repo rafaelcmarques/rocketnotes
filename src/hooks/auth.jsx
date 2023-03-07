e91fa89fd0f51083b312d4b47bf0da2372810ce5
Rafael Marques <rafael@RAFAEL-PC>
2023-03-06 21:27:40 -0300

    implementado mudança de rota

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,15 +1,22 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState, useEffect } from "react"
 
 import { api } from "../services/api"
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+  const [data, setData] = useState({})
+
   async function signIn({ email, password }) {
-    console.log({ email, password })
     try {
       const response = await api.post("/sessions", { email, password })
-      console.log(response)
+      const { user, token } = response.data
+
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+      localStorage.setItem("@rocketnotes:token", token)
+
+      api.defaults.headers.authorization = `Bearer ${token}`
+      setData({ user, token })
     } catch (error) {
       if (error.response) {
         alert(error.response.data.message)
@@ -19,8 +26,23 @@ function AuthProvider({ children }) {
     }
   }
 
+  useEffect(() => {
+    const user = localStorage.getItem("@rocketnotes:user")
+    const token = localStorage.getItem("@rocketnotes:token")
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+      setData({
+        token,
+        user: JSON.parse(user),
+      })
+    }
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ signIn }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
+      {children}
+    </AuthContext.Provider>
   )
 }
 
